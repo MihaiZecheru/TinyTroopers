a581fae596ba2571d56d2927a4cb61d504478533
Mihai Zecheru <zecheruchris@gmail.com>
2024-09-24 23:10:40 -0700

    Add screen blueprint
    Add abstract Screen class to act as a blueprint for all screen classes

---
+++ src/TinyTroopers/Screens/Screen.java
@@ -0,0 +1,37 @@
+package TinyTroopers.Screens;
+
+import javax.swing.*;
+
+/**
+ * Screen base class. Represents a window screen in the GUI app.
+ */
+public abstract class Screen extends JPanel {
+    protected final JFrame w;
+
+    /**
+     * Default initialization is setting the main window reference
+     *
+     * @param window A reference to the main window for which this screen is to be displayed
+     */
+    public Screen(JFrame window) {
+        this.w = window;
+        InitializeScreen();
+    }
+
+    /**
+     * Initialize the screen
+     */
+    protected abstract void InitializeScreen();
+
+    /**
+     * Display the screen to the main window
+     *
+     * @return The object for chaining purposes
+     */
+    public Screen Display() {
+        this.w.setContentPane(this);
+        this.w.revalidate();
+        this.w.repaint();
+        return this;
+    }
+}
